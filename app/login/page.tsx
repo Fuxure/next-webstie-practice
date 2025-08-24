2d34f4ce8b053975c1f1e3d917d92e7c81216448
Fu <frederic010992@gmail.com>
2025-08-24 14:42:11 -0500

    Finish

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,9 @@
+import type { Metadata } from "next";
 import AcmeLogo from "@/app/ui/acme-logo";
 import LoginForm from "@/app/ui/login-form";
 import { Suspense } from "react";
+
+export const metadata: Metadata = { title: "Login" };
 
 export default function LoginPage() {
   return (
