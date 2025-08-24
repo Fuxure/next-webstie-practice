8bdc910180275b1cf8131746f4df50ceb6dc2510
Fu <frederic010992@gmail.com>
2025-08-24 14:21:03 -0500

    chapter 15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,7 +4,10 @@ import { z } from "zod";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import postgres from "postgres";
-const sql = postgres(process.env.PSOTGRES_URL!, { ssl: "require" });
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
   id: z.string(),
@@ -24,7 +27,7 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export type State = {
   errors?: {
-    customerid?: string[];
+    customerId?: string[];
     amount?: string[];
     status?: string[];
   };
@@ -96,7 +99,29 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error("Failed to Delete Invoice");
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath("/dashboard/invoices");
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath("/dashboard/invoices");
+  } catch (error) {
+    throw new Error("Failed to Delete Invoice");
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
 }
